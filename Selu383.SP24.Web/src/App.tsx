042891a80160827dead42a49b27d6643bc1a79f0
henryperrien <henry.perrien@selu.edu>
2024-02-22 11:55:00 -0600

    basic login logout

--- Selu383.SP24.Web/src/App.tsx
+++ Selu383.SP24.Web/src/App.tsx
@@ -1,10 +1,52 @@
-import { useState } from 'react'
+import { useState, ChangeEvent, FormEvent, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+interface UserDto {
+	userName?: string;
+	id?: number;
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState<UserDto | null>(null);
+
+  function handleUserNameChange(e: ChangeEvent<HTMLInputElement>){
+    setUsername(e.target.value);
+  }
+
+  function handleSignout(){
+    return fetch("/api/authentication/logout",{method: "POST"}).then(async x => setUser(null));
+    }
+
+  function handlePasswordChange(e: ChangeEvent<HTMLInputElement>){
+    setPassword(e.target.value);
+  }
+  
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    
+    fetch("/api/authentication/login", {
+        method: 'POST',
+        headers: { "Content-Type": 'application/json' },
+        body: JSON.stringify({
+            userName: username,
+            password: password,
+        })
+    }).then(async x => {
+        const userResp = await x.json();
+        setUser(userResp);
+    });
+}
+
+  useEffect(() => {
+    fetch("/api/authentication/me").then(async (x) =>{
+      x.json().then((userResp) => setUser(userResp))
+    });
+  }, [])
 
   return (
     <>
@@ -17,14 +59,18 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      {user !== null ? <div>Hello<button onClick = {handleSignout}>Logout</button></div>  :
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <form action="/api/authentication/login" method="POST" 
+        style ={{ display: "flex", flexDirection: "column" }} onSubmit={handleSubmit}>
+          <label htmlFor='username'>Username</label>
+          <input type = "text" name = "username" id = "username" value = {username} onChange = {handleUserNameChange}/>
+          <label htmlFor='password'>Password</label>
+          <input type = "password" name = "password" id = "password" value = {password} onChange = {handlePasswordChange}/>
+          <button type="submit">Log in</button>
+        </form>
       </div>
+}
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
@@ -32,4 +78,9 @@ function App() {
   )
 }
 
+
 export default App
+function async(x: any): ((value: Response) => Response | PromiseLike<Response>) | null | undefined {
+  throw new Error('Function not implemented.');
+}
+
