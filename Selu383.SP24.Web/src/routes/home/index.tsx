5c2cc6788cd45c3344937637670bcfbc624f5f67
Dmytro Levytskyi <Dmytro.levytskyi@selu.edu>
2024-02-27 15:31:41 -0600

    home page serach bar
    added:
    serch bar,
    css for the search bar

--- Selu383.SP24.Web/src/routes/home/index.tsx
+++ Selu383.SP24.Web/src/routes/home/index.tsx
@@ -1,12 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Card, Button } from 'react-bootstrap';
+import '../../styles/home.css';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
-export default function Home() {
+const HotelSearchBar = () => {
+  const today = new Date();
+  const tomorrow = getNextDay(today);
+  const [checkInDate, setCheckInDate] = useState(today);
+  const [checkOutDate, setCheckOutDate] = useState(tomorrow);
+  const [numGuests, setNumGuests] = useState(1);
+  const [numRooms, setNumRooms] = useState(1);
+
+  const handleSearch = () => {
+    // Handle search functionality here
+    console.log('Search with parameters:', {
+      checkInDate,
+      checkOutDate,
+      numGuests,
+      numRooms
+    });
+  };
+
+  function getNextDay(date: Date) {
+    const nextDay = new Date(date);
+    nextDay.setDate(date.getDate() + 1);
+    return nextDay;
+  }
+
   return (
     <>
     <center><h1>Welcome to EnStay!</h1></center>
-      
+    <div className="wrapper">
+    <div className="hotel-search-bar">
+      <DatePicker
+        selected={checkInDate}
+        onChange={date => setCheckInDate(date || today)}
+        selectsStart
+        startDate={checkInDate}
+        endDate={checkOutDate}
+        placeholderText="From"
+      />
+      <DatePicker
+        selected={checkOutDate}
+        onChange={date => setCheckOutDate(date || tomorrow)}
+        selectsEnd
+        startDate={checkInDate}
+        endDate={checkOutDate}
+        minDate={checkInDate}
+        placeholderText="To"
+      />
+      <select
+        value={numGuests}
+        onChange={(e) => setNumGuests(parseInt(e.target.value))}
+      >
+        {[...Array(10).keys()].map(num => (
+          <option key={num} value={num+1}>{num+1} Guest(s)</option>
+        ))}
+      </select>
+      <select
+        value={numRooms}
+        onChange={(e) => setNumRooms(parseInt(e.target.value))}
+      >
+        {[...Array(5).keys()].map(num => (
+          <option key={num} value={num+1}>{num+1} Room(s)</option>
+        ))}
+      </select>
+      <button onClick={handleSearch}>Search</button>
+    </div>
+    </div>
       
       <center>
         <Link to="/hotels">
@@ -15,3 +78,4 @@ export default function Home() {
     </>
   );
 }
+export default HotelSearchBar;
