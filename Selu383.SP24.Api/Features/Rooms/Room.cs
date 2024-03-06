63c94aa3531809f4e1add794a265662c51521221
henryperrien <henry.perrien@selu.edu>
2024-03-05 20:32:44 -0600

    creating room table

---
+++ Selu383.SP24.Api/Features/Rooms/Room.cs
@@ -0,0 +1,28 @@
+﻿using Selu383.SP24.Api.Features.Hotels;
+
+namespace Selu383.SP24.Api.Features.Rooms
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public int HotelId { get; set; }
+        public Hotel hotel { get; set; }
+        public int Rate { get; set; }
+        public int RoomNumber { get; set; }
+    }
+
+    public class RoomDto
+    {
+        public int Id { get; set; }
+        public int HotelId { get; set; }
+        public int Rate { get; set; }
+        public int RoomNumber { get; set; }
+    }
+
+    public class RoomUpdateDto
+    {
+        public int HotelId { get; set; }
+        public int Rate { get; set; }
+        public int RoomNumber { get; set; }
+    }
+}
