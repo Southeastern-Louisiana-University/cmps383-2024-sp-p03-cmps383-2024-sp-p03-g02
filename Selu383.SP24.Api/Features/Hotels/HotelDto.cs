7e5e43d48f91193b90b6d2a581fa1e3dd9bb6105
henryperrien <henry.perrien@selu.edu>
2024-03-11 17:38:21 -0500

    change hoteldto to have citydto as location

--- Selu383.SP24.Api/Features/Hotels/HotelDto.cs
+++ Selu383.SP24.Api/Features/Hotels/HotelDto.cs
@@ -1,4 +1,6 @@
-﻿namespace Selu383.SP24.Api.Features.Hotels;
+﻿using Selu383.SP24.Api.Features.Cities;
+
+namespace Selu383.SP24.Api.Features.Hotels;
 
 public class HotelDto
 {
@@ -10,6 +12,7 @@ public class HotelDto
 
     public int? ManagerId { get; set; }
     public int? LocationId { get; set; }
+    public CityDto Location { get; set; }
     public string Email { get; set; }
     public string ContactNumber { get; set; }
     public string Image { get; set; }
