e3783230a73bf85cb24d47bb5cd238b4b35af9b1
Sheylo1 <wesley.austin-2@selu.edu>
2024-03-11 12:03:46 -0500

    STYLING, CHANGED PLACEHOLDER IMAGE

--- Selu383.SP24.Web/src/routes/hotels/index.tsx
+++ Selu383.SP24.Web/src/routes/hotels/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 interface HotelDto {
   id?: number;
@@ -11,7 +11,6 @@ interface HotelDto {
 
 const HotelListPage: React.FC = () => {
   const [hotels, setHotels] = useState<HotelDto[]>([]);
-  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchHotels = async () => {
@@ -28,11 +27,11 @@ const HotelListPage: React.FC = () => {
   }, []);
 
   return (
-    <Container className="mt-4">
-      <h1 className="text-center mb-4">Hotels</h1>
+    <Container fluid className="mt-4">
+      <h1 className="text-center mb-4" style={{ fontSize: '3rem', fontWeight: 'bold', textShadow: '2px 2px 5px #FDBA74' }}>Hotels</h1>
       <Row className="justify-content-center">
-        {hotels.map((hotel) => (
-          <Col key={hotel.id} md={3} className="mb-3">
+        {hotels.map((hotel,) => (
+          <Col key={hotel.id} xs={12} sm={6} md={4} lg={3} className="mb-3">
             <Link to={`/hotels/${hotel.id}`} style={{ textDecoration: 'none' }}>
               <Card
                 style={{
@@ -42,15 +41,15 @@ const HotelListPage: React.FC = () => {
                 }}
                 className="shadow"
               >
-                {/* Placeholder image */}
+                {/* Placeholder image below!!!! */}
                 <Card.Img
                   variant="top"
-                  src="https://imgur.com/f6DUjOS.png"
+                  src="https://imgur.com/MLARzB8.png"
                   alt="Hotel Placeholder"
                 />
 
                 <Card.Body>
-                  <Card.Title style={{ color: '#FDBA74' }}>{hotel.name}</Card.Title>
+                  <Card.Title style={{ color: '#000000', fontWeight: 'bold', textShadow: '2px 2px 5px #FDBA74' }}>{hotel.name}</Card.Title>
                   <Card.Subtitle className="mb-2 text-muted">{hotel.address}</Card.Subtitle>
                 </Card.Body>
               </Card>
