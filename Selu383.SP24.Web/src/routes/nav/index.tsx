318954f3e312e71a316dca303bfa59d1fc676b1e
Kyle Brown <you@example.com>
2024-04-17 15:41:19 -0500

    WIP admin login on new home page

--- Selu383.SP24.Web/src/routes/nav/index.tsx
+++ Selu383.SP24.Web/src/routes/nav/index.tsx
@@ -2,7 +2,6 @@ import { Link, Outlet } from "react-router-dom";
 import { useState, useEffect } from 'react';
 import { Button, Navbar } from 'react-bootstrap';
 
-
 interface UserDto {
     userName?: string;
     id?: number;
@@ -13,9 +12,10 @@ export default function NavbarLayout() {
     const [searchTerm, setSearchTerm] = useState("");
 
     function handleSignout() {
-        return fetch("/api/authentication/logout", { method: "POST" }).then(async () => setUser(null));
+        return fetch("/api/authentication/logout", { method: "POST" })
+            .then(async () => setUser(null));
     }
-    
+
     useEffect(() => {
         fetch("/api/authentication/me").then(async (x) => {
             x.json().then((userResp) => setUser(userResp));
@@ -24,32 +24,56 @@ export default function NavbarLayout() {
 
     return (
         <>
-            <Navbar className="navbar navbar-light bg-light" style={{ position: 'sticky', top: 0, zIndex: 1000 }}>
-                <Link to={'/'}><span style={{ color: '#FDBA74' }} className="navbar-brand mb-0 h1">EnStay</span></Link>
-                <Link to={'/Hotels'}><Button style={{ border: "1px solid black"} } className="btn-light">Hotels</Button></Link>
+            <Navbar className="navbar navbar-light bg-light">
+                <Link to={'/'}>
+                    <span style={{ color: '#FDBA74' }} className="navbar-brand mb-0 h1">EnStay</span>
+                </Link>
+
+                <Link to={'/Hotels'}>
+                    <Button style={{ border: "1px solid black" }} className="btn-light">Hotels</Button>
+                </Link>
+
                 <div className="d-flex justify-content-center align-items-center flex-grow-1">
-                    <input id="search" name="search" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value ?? "")} className="form-control mx-2" style={{ maxWidth: '300px' }} />
+                    <input
+                        id="search"
+                        name="search"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value ?? "")}
+                        className="form-control mx-2"
+                        style={{ maxWidth: '300px' }}
+                    />
                     <Link
                         onClick={(e) => (!searchTerm ? e.preventDefault() : null)}
                         to={`/find-hotel?searchTerm=${encodeURIComponent(searchTerm)}&start=now`}
                         aria-disabled={!searchTerm}
                     >
-                        <Button style={{ backgroundColor: '#FDBA74' }} className="btn-light">Basic Search</Button>
+                        <Button style={{ backgroundColor: "#FDBA74", color: "#FFFFFF" }} className="btn-light">Search</Button>
                     </Link>
                 </div>
+
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
-                    {user === null && ( // Render the admin login button only if no user is logged in
-                        <Link to={'/AdminLogin'}><Button style={{ backgroundColor: '#FDBA74' }} className="btn-light">Admin Login</Button></Link>
-                    )}
                         {user !== null ? (
-                            <Button style={{ backgroundColor: '#FDBA74' }} className="btn-light" onClick={handleSignout}>Logout</Button>
+                            <div>
+                                <Link to={'/userbooking'}>
+                                    <Button style={{ backgroundColor: '#FDBA74', color: "#FFFFFF" }} className="btn-light">Bookings</Button>
+                                </Link>
+                                <Button style={{ backgroundColor: '#FDBA74', color: "#FFFFFF" }} className="btn-light" onClick={handleSignout}>Logout</Button>
+                            </div>
                         ) : (
-                            <Link to={'/Login'}><Button style={{ backgroundColor: '#FDBA74' }} className="btn-light">Login</Button></Link>
+                            <div>
+                                <Link to={'/AdminLogin'}>
+                                    <Button style={{ backgroundColor: '#FDBA74', color: "#FFFFFF", marginLeft: "10px" }} className="btn-light">Admin Login</Button>
+                                </Link>
+                                <Link to={'/Login'}>
+                                    <Button style={{ backgroundColor: '#FDBA74', color: "#FFFFFF" }} className="btn-light">Login</Button>
+                                </Link>
+                            </div>
                         )}
                     </li>
                 </ul>
             </Navbar>
+
             <Outlet />
         </>
     );
